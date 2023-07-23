24b7e68f151886d1a5b01285948604785838b396
Marcel Renz <marcel.renz@freenet.de>
2023-07-23 21:09:04 +0200

    use new data_set

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,24 @@ impl Point {
 }
 
 pub fn do_it() {
+
+    let data_set = include_str!("../test_data/global_terrorism_stripped.csv");
+    let data_set = data_set
+    .lines()
+    .enumerate()
+    .filter(|(_, line)| line.split(",").count() > 1)
+    .map(|(idx, line)| {
+        line.split(",")
+            .take(2)
+            .map(|value| value.parse::<f64>().expect(format!("WTF in line {}", idx).as_str()))
+            .collect::<Vec<f64>>()
+    })
+    // file has latitude first, but longitude represents x and latitude y so we have to swap it
+    .map(|split_line| Point::new(split_line[1], split_line[0]))
+    .collect();
+
+    /*
+    McDonalds 
     let data_set = include_str!("../test_data/mcdonalds_stripped.csv");
     let data_set = data_set
         .lines()
@@ -26,7 +44,7 @@ pub fn do_it() {
         // file has latitude first, but longitude represents x and latitude y so we have to swap it
         .map(|split_line| Point::new(split_line[1], split_line[0]))
         .collect();
-
+*/
     //DBSCAN Test
 
     /*
